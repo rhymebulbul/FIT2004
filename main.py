b911f03def1767bed218eb6873a2d9c7e593e5e6
rhyme <rhyme1325@gmail.com>
2022-08-18 17:38:54 +1000

    Complete documentation

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ https://www.youtube.com/watch?v=OKd534EWcdk
 
 def analyze(results, roster, score):
     """
-    Basic function to consider opposite matches, sort data, remove duplicates and then call appropriate functions
-    to retrieve top ten matches and searched matches
+    Basic function to consider opposite matches, sort data, remove duplicates
+    and then call appropriate functions to retrieve top ten matches and searched matches
     :param results: List of lists, containing each match
     :param roster: Number length of character set teams are formed of
     :param score: The minimum score to look for in a match
     :return: List of lists containing top ten matches and searched matches
-    :Time complexity:
-    :Aux Space complexity:
+    :Time complexity: O(NM)
+    :Aux Space complexity: O(NM)
     """
     # Add opposites to mix
     addOpposites(results)
@@ -24,112 +24,116 @@ def analyze(results, roster, score):
     sort(results, roster)
     # Remove duplicates so all are unique
     uniques = sameScoreDuplicates(results, roster)
-
     # Get matches with at least the searched score
     searchedMatches = getSearchedMatches(uniques, score)
-
     # Get the top ten matches for this data set
     topTenMatches = getTopTenMatches(uniques)
-
     return [topTenMatches, searchedMatches]
 
 def getTopTenMatches(array):
     """
-    High level description about the functiona and the approach
+    Gets the top ten matches with highest scores
+    Takes the first ten matches from the sorted array
+    Sorts each team name into lexicographical order
     :Input:
-        argv1:
-        argv2:
-    :Output, return or postcondition:
-    :Time complexity:
-    :Aux space complexity:
+        argv1: array of results
+    :Return: List of top ten matches with highest scores
+    :Time complexity: O(NM)
+    :Aux space complexity: O(NM)
     """
     topTenMatches = []
-
+    # Iterates over array and adds first ten, which are actually greatest ten scores
     for i in range(len(array) - 1, len(array) - 11, -1):
         topTenMatches.append(array[i])
-
+    # Sorts each team name into lexicographical order
     sortTeam(topTenMatches)
-
     return topTenMatches
 
 
 def getSearchedMatches(array, score):
     """
-    High level description about the functiona and the approach
+    Get all matches with target score, or nearest higher score if not found.
+    Iterates array for target score or higher,
+    Once found, appends and sets that to target score, and only looks for exact score
+    Sorts each team name into lexicographical order
     :Input:
-        argv1:
-        argv2:
-    :Output, return or postcondition:
-    :Time complexity:
-    :Aux space complexity:
+        argv1: array of results
+        argv2: minimum score we are targeting
+    :Return: list of matches with at least the target score
+    :Time complexity: O(NM)
+    :Aux space complexity: O(NM)
     """
     matchFound = False
     searchedMatches = []
-
+    # Iterate over array until the score or greater is found
     for j in range(len(array)):
-        if array[j][2] >= score and not matchFound:
-            score = array[j][2]
-            matchFound = True
-            searchedMatches.append(array[j])
-        elif array[j][2] == score and matchFound:
-            searchedMatches.append(array[j])
-
+        if array[j][2] >= score and not matchFound: # Once score or greater is found
+            score = array[j][2] # set that to target score
+            matchFound = True # stop looking for at least score, and look for exact score only
+            searchedMatches.append(array[j]) # add element to list
+        elif array[j][2] == score and matchFound: # Only look for exact score
+            searchedMatches.append(array[j]) # add element to list
+    # Sorts each team name into lexicographical order
     sortTeam(searchedMatches)
     return searchedMatches
 
 def addOpposites(array):
     """
-    High level description about the functiona and the approach
+    Adds in scores of the opposite teams to the array
+    Calls getRotatedMatch() to get the opposite match and
+    appends to end of array
     :Input:
-        argv1:
-        argv2:
+        argv1: array of results
     :Output, return or postcondition:
-    :Time complexity:
-    :Aux space complexity:
+    :Time complexity: O(N)
+    :Aux space complexity: O(N)
     """
     for j in range(len(array)):
         array.append(getRotatedMatch(array[j]))
 
 def sameScoreDuplicates(array, roster):
     """
-    High level description about the functiona and the approach
+    Removes duplicate data from the array ie. same teams with the same score.
+    Iterates over array and compares previous element. Copies over if unique.
+    Does not if duplicate
     :Input:
-        argv1:
-        argv2:
-    :Output, return or postcondition:
-    :Time complexity:
-    :Aux space complexity:
+        argv1: array of results
+        argv2: Character set team is formed from
+    :Return: array of unique elements
+    :Time complexity: O(N)
+    :Aux space complexity: O(N)
     """
     j = 0
+    # Iterate over matches in results array and compare team and score with previous ones
     for i in range(1, len(array)):
         if not isTeamSame(array[i][0], array[i - 1][0], roster) or not isTeamSame(array[i][1], array[i - 1][1], roster) \
-                or array[i][2] != array[i - 1][2]:
-            array[j + 1] = array[i]
+                or array[i][2] != array[i - 1][2]: # copy over match if unique
+            array[j + 1] = array[i] # Uniques[0...j], Duplicates[j+1...n]
             j += 1
-
+    # Create new array with uniques only
     output = []
-    for i in range(j+1):
+    for i in range(j+1): # Only copy upto j, which are unique elements
         output.append(array[i])
-
     return output
 
 def sort(array, roster):
     """
-        High level description about the functiona and the approach
-        :Input:
-            argv1:
-            argv2:
-        :Output, return or postcondition:
-        :Time complexity:
-        :Aux space complexity:
-        """
-    # Sort in alphabetical order of second team
+    Sorts results array as per requirements.
+    Firsts sort in lexicographical order of team2 so that it remains in place
+    Then team1, which also remains in place when finally sorted in order of scores
+    :Input:
+        argv1: array of results
+        argv2: Character set team is formed from
+    :Postcondition: array is sorted as per requirements
+    :Time complexity: O(NM)
+    :Aux space complexity: O(NM)
+    """
+    # Sort in lexicographical order of second team
     radixSortRoster(array, roster, 1)
-    # Sort in alphabetical order of first team
+    # Sort in lexicographical order of first team
     radixSortRoster(array, roster, 0)
     # Now sort in order of scores with inplace sort
     radixSort(array)
-
 
 def countingSort(array, place):
     """
@@ -138,7 +142,7 @@ def countingSort(array, place):
     :Input:
         argv1: Array to be sorted
         argv2: The ones, tens or hundreds place we are sorting by
-    :Output, return or postcondition: Array is sorted at given place
+    :Postcondition: Array is sorted at given place
     :Time complexity: O(M)
     :Aux space complexity: O(M)
     """
@@ -169,7 +173,7 @@ def radixSort(array):
     Calls counting sort on each place of team score till array is sorted
     :Input:
         argv1: Array of results to sort
-    :Output, return or postcondition: Array is sorted
+    :Postcondition: Array is sorted
     :Time complexity: O(M)
     :Aux space complexity: O(M)
     """
@@ -189,7 +193,7 @@ def countingSortRoster(array, roster, place, team):
         argv2: Character set team is formed from
         argv1: The ones, tens or hundreds place we are sorting by
         argv2: Team we want to sort based on
-    :Output, return or postcondition: Array is sorted at given place
+    :Postcondition: Array is sorted at given place
     :Time complexity: O(M)
     :Aux space complexity: O(M)
     """
@@ -222,9 +226,9 @@ def radixSortRoster(array, roster, team):
         argv1: Array of results to sort
         argv2: Character set team is formed from
         argv3: Team we want to sort based on
-    :Output, return or postcondition: Array is sorted
-    :Time complexity: O(M)
-    :Aux space complexity: O(M)
+    :Postcondition: Array is sorted
+    :Time complexity: O(NM)
+    :Aux space complexity: O(NM)
     """
     place = len(array[0][0]) - 1
     # Continue to call counting sort on each place of the numbers until sorted
@@ -239,7 +243,7 @@ def countingSortTeam(array, place):
     :Input:
         argv1: Array to be sorted
         argv2: The ones, tens or hundreds place we are sorting by
-    :Output, return or postcondition: Array is sorted at given place
+    :Postcondition: Array is sorted at given place
     :Time complexity: O(M)
     :Aux space complexity: O(M)
     """
@@ -270,7 +274,7 @@ def radixSortTeam(array):
     Calls counting sort on each place of numbers till array is sorted
     :Input:
         argv1: Team name we want to sort
-    :Output, return or postcondition: Array is sorted
+    :Postcondition: Array is sorted
     :Time complexity: O(M)
     :Aux space complexity: O(M)
     """
@@ -283,10 +287,10 @@ def radixSortTeam(array):
 
 def sortByChar(team):
     """
-    Takes a team name and sorts in alphabetical order
+    Takes a team name and sorts in lexicographical order
     :Input:
         argv1: Team name we want to sort
-    :Output, return or postcondition: Team name sorted in alphabetical order
+    :Return: Team name sorted in lexicographical order
     :Time complexity: O(M)
     :Aux space complexity: O(M)
     """
@@ -308,9 +312,9 @@ def sortTeam(array):
     Iterates list and calls sort on each team name
     :Input:
         argv1: The results list we want the team characters rearranged
-    :Output, return or postcondition: The given list now has team names sorted in alphabetical order
-    :Time complexity: N?
-    :Aux space complexity:
+    :Postcondition: The given list now has team names sorted in lexicographical order
+    :Time complexity: O(NM)
+    :Aux space complexity: O(NM)
     """
     for i in range(len(array)):
         array[i][0] = sortByChar(array[i][0])
@@ -328,19 +332,6 @@ def getRotatedMatch(match):
     :Aux space complexity: O(1)
     """
     return [match[1], match[0], 100 - match[2]]
-
-# def rotateMatch(array, result):
-#     """
-#     High level description about the functiona and the approach
-#     :Input:
-#         argv1:
-#         argv2:
-#     :Output, return or postcondition:
-#     :Time complexity:
-#     :Aux space complexity:
-#     """
-#     temp = [array[result][1], array[result][0], 100 - array[result][2]]
-#     array[result] = temp
 
 def isTeamSame(a, b, roster):
     """
